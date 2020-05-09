8a46952e670f94df9cccbe83fb0be850fbdfce84
evertonProgramadorCriativo <evertonprogramadorcriativo@gmail.com>
2020-05-09 14:17:25 -0300

    redux , thunk

---
+++ pequenoblog/src/actions/store.js
@@ -0,0 +1,11 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from "redux-thunk";
+
+export const store =  createStore (
+   {},
+   compose(
+         applyMiddleware(thunk)
+   )
+   
+   )
+
