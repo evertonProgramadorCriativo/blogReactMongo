4368f5e95bd8cee2c8c012fce32eda5b0963a457
evertonProgramadorCriativo <evertonprogramadorcriativo@gmail.com>
2020-05-09 16:35:44 -0300

    api.js

--- pequenoblog/src/actions/postMessage.js
+++ pequenoblog/src/actions/postMessage.js
@@ -1,7 +1,7 @@
 export const ACTION_TYPES = {
-    CREATE: 'CREATE'
-    UPDATE: 'UPDATE'
-    DELETE: 'DELETE'
+    CREATE: 'CREATE',
+    UPDATE: 'UPDATE',
+    DELETE: 'DELETE',
     FETCH_ALL: 'FETCH_ALL'
 }
 
