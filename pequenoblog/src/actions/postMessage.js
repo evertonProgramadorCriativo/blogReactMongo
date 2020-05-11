b1ad1313a038ea501d19d8a3222d4a02edb362fd
evertonProgramadorCriativo <evertonprogramadorcriativo@gmail.com>
2020-05-11 20:49:15 -0300

    create api full

--- pequenoblog/src/actions/postMessage.js
+++ pequenoblog/src/actions/postMessage.js
@@ -1,24 +1,34 @@
 import api from "./api.js";
 
-export  const ACTION_TYPES = {
+export const ACTION_TYPES = {
   CREATE: "CREATE",
   UPDATE: "UPDATE",
   DELETE: "DELETE",
-  FETCH_ALL: "FETCH_ALL"
-}
+  FETCH_ALL: "FETCH_ALL",
+};
 
-export const fetchAll = () => dispatch => {
-  
-  api.postMessage().fetchAll().then(res => {
-       dispatch({
+export const fetchAll = () => (dispatch) => {
+  api
+    .postMessage()
+    .fetchAll()
+    .then((res) => {
+      dispatch({
         type: ACTION_TYPES.FETCH_ALL,
-        payload: res.data
-      })
+        payload: res.data,
+      });
     })
     .catch((err) => console.log(err));
-}
+};
 
-
-export const create = (data, onSuccess)=>dispatch => {
-
-}
+export const create = (data, onSuccess) => (dispatch) => {
+  api
+    .postMessage()
+    .create(data)
+    .then((res) => {
+      dispatch({
+        type: ACTION_TYPES.CREATE,
+        payload: res.data,
+      });
+    })
+    .catch((err) => console.log(err));
+};
